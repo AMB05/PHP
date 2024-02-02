96450bdf68e41071433dcd1208a2c4573b7c1667
Arriva <92034212+AMB05@users.noreply.github.com>
2024-02-03 03:31:28 +0700

    Aritmatika_Operation
    Aritmatika_Operation

---
+++ Operasi_Aritmatika.php
@@ -0,0 +1,20 @@
+<?
+$a=10;
+$b=3;
+echo "\$a =$a <BR>";
+echo "\$b =$b <BR>";
+echo '$a + $b =';
+print $a+$b;
+echo "<BR>";
+echo '$a-$b = ';
+print $a-$b;
+echo "<BR>";
+echo '$a*$b = ';
+print $a*$b;
+echo "<BR>";
+echo '$a/$b = ';
+print $a/$b;
+echo "<BR>";
+echo '$a%$b = ';
+print $a%$b;
+?>
