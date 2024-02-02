cdf343c7dad702e4be5e536d998d47e7f46356bd
Arriva <92034212+AMB05@users.noreply.github.com>
2024-02-03 03:33:20 +0700

    Variabels
    Variabels

---
+++ Varibael.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>
+        Contoh Skrip PHP
+    </title>
+    <body>
+        <?
+        $nama = "Arriva";
+        echo "$nama berkata\"  Katakan Namamu!\"";
+        ?>
+    </body>
+</head>
+</html>
